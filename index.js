5022ca378fbb4169d949bb5f8efdc57ca5efeab4
Yuri Bekmetov <bekm.yu@yandex.ru>
2023-01-30 17:53:45 +0500

    routes added

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from "./routes/posts.js";
+
 const app = express();
+app.use(cors());
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
 
 const CONNECTION_URL = "mongodb+srv://admin:admin@cluster.gjw5pn2.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
