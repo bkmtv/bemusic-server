5c26faedc64216ad6010403cc333c729f3226c54
Yuri Bekmetov <bekm.yu@yandex.ru>
2023-02-04 02:30:16 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 require('dotenv').config();
 
 app.use(express.json());
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "https://collections-ibkmt.vercel.app" }));
 
 const db = require("./models");
 
